cf6f09f07f07ec34386a00e09fbf1a17b5352e4e
Martin Gergov <martindgergov@gmail.com>
2024-03-11 18:56:21 +0100

    Eliminar archivos innecesarios

--- src/db.ts
+++ src/db.ts
@@ -6,14 +6,6 @@ const db = pgp("postgres://martingergov:phonicenvelope@localhost:5432/planets");
 
 const setUpDb = async () => {
   await db.none(`
-    DROP TABLE IF EXISTS planets;
-
-    CREATE TABLE planets (
-        id SERIAL NOT NULL PRIMARY KEY,
-        name TEXT NOT NULL,
-        image TEXT
-    );
-
     DROP TABLE IF EXISTS users;
 
     CREATE TABLE users (
@@ -23,8 +15,6 @@ const setUpDb = async () => {
         token TEXT
     )`);
 
-  await db.none(`INSERT INTO planets (name) VALUES ('Earth')`);
-  await db.none(`INSERT INTO planets (name) VALUES ('Mars')`);
   await db.none(
     `INSERT INTO users (username, password) VALUES ('usuario', 'ususario')`
   );
--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,6 @@ import "express-async-errors";
 import morgan from "morgan";
 import multer from "multer";
 import "./passport.js";
-import {
-  getAll,
-  getOneById,
-  create,
-  updateById,
-  deleteById,
-  createImage,
-} from "./controllers/planets.js";
 import { signUp, logIn, logOut } from "./controllers/users.js";
 import authorize from "./authorize.js";
 
@@ -30,13 +22,6 @@ const port = 3001;
 
 app.use(morgan("dev"));
 app.use(express.json());
-
-app.get("/api/planets", getAll);
-app.get("/api/planets/:id", getOneById);
-app.post("/api/planets", create);
-app.put("/api/planets/:id", updateById);
-app.delete("/api/planets/:id", deleteById);
-app.post("/api/planets/:id/image", upload.single("image"), createImage);
 
 app.post("/api/users/signup", signUp);
 app.post("/api/users/login", logIn);
